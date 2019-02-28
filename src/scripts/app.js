6bf87ef0c08edee4ed0ba7277bf260f3d84f6fc2
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-28 14:41:18 +0100

    Add script to change active button favorite & add to compare on click

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,19 @@
-console.log('hello');
+(function () {
+    "use strict";
+  
+    //Change active button favorite & add to compare on click
+    const btnFavorite = document.querySelectorAll(".btn-outline");
+  
+    btnFavorite.forEach(element => {
+      element.addEventListener("click",
+        function (event) {
+          if (this.classList.contains("active")) {
+            this.classList.remove("active");
+          } else {
+            this.className += " active";
+          }
+          event.preventDefault();
+        })
+    });
+  
+  })();
