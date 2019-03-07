7c6d95af8e332e157d30cb2d6c57a46d3acf4797
Łukasz Król <lionel@o2.pl>
2019-03-07 18:37:08 +0100

    Add script for responsive menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
-console.log('hello');
+(function () {
+  var showMenuBtn = document.querySelector('.menu-list-display');
+  var menuList = document.getElementById('dropdown-menu');
+
+  showMenuBtn.addEventListener('click', function () {
+    menuList.classList.toggle('dropdown');
+    showMenuBtn.classList.toggle('active');
+  });
+})();
