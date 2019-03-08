a661201defe3185b445b4e3855d917eed9366cdf
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-03-08 12:11:27 +0100

    Add carousel -tiny-slider to new furniture

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,40 @@
-(function () {
+(function() {
   'use strict';
 
   // Change active button favorite & add to compare on click
   const btnFavorite = document.querySelectorAll('.btn-outline');
 
   btnFavorite.forEach(element => {
-    element.addEventListener('click', function (event) {
+    element.addEventListener('click', function(event) {
       this.classList.toggle('active');
       event.preventDefault();
     });
+  });
+
+  // Add dots to menu as carousel nav
+  function ChangeDotsNumber() {
+    let pages;
+    let carouselDots = document.querySelector('.carousel-dots');
+    let sliderItems = document.querySelectorAll('.slider-item');
+    let vPortWidth = window.innerWidth || document.documentElement.clientWidth;
+    // Reset number
+    carouselDots.innerHTML = '';
+    // Numbers of dots RWD
+    if (vPortWidth >= 992) {
+      pages = sliderItems.length / 3;
+    } else if (vPortWidth >= 572) {
+      pages = sliderItems.length / 2;
+    } else if (vPortWidth <= 567) {
+      pages = sliderItems.length;
+    }
+
+    for (let i = 0; i < pages; i++) {
+      carouselDots.insertAdjacentHTML('afterbegin', '<li><a></a></li>');
+    }
+  }
+  ChangeDotsNumber();
+  // Change number when resized
+  window.addEventListener('resize', function() {
+    ChangeDotsNumber();
   });
 })();
---
+++ src/scripts/carousel.js
@@ -0,0 +1,25 @@
+let slider = tns({
+  container: '.my-slider',
+  navContainer: '.carousel-dots',
+  items: 1,
+  controls: false,
+  nav: true,
+  responsive: {
+    572: {
+      items: 2,
+      gutter: 5
+    },
+    992: {
+      items: 3
+    }
+  },
+  slideBy: 'page',
+  mouseDrag: true,
+  swipeAngle: false,
+  loop: false
+});
+
+// Slider rebuild when window resize
+window.addEventListener('resize', function() {
+  slider.rebuild();
+});
