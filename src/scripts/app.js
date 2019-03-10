5b9a5784f62a6623f3d724cf28123e1f6575cd51
ArkadiuszGarzel <ejkej87@gmail.com>
2019-03-10 22:06:47 +0100

    work in progress

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,90 @@
 console.log('hello');
+
+document.addEventListener('DOMContentLoaded', function () {
+  tabsy();
+});
+
+function tabsy () {
+  let tabs = document.querySelectorAll('.tabs');
+  let links = document.querySelectorAll('.panel-bar .menu ul li a');
+  let data = '';
+  //  let index = 0;
+  let elements = document.querySelectorAll('.dot');
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function (e) {
+      e.preventDefault();
+
+      for (let sibling of this.parentElement.parentElement.children) {
+        if (sibling !== this && sibling.childNodes[0].nodeType === 1) {
+          //           console.log(sibling.childNodes[0].nodeType);
+          sibling.childNodes[0].classList.remove('active');
+        }
+      }
+      this.classList.add('active');
+      data = this.dataset.anchor;
+      var numbered = this.dataset.number;
+      //       console.log(this.dataset.number);
+
+      for (let j = 0; j < tabs.length; j++) {
+        tabs[j].classList.remove('active');
+        setTimeout(function () {
+          tabs[j].style.display = 'none';
+        }, 750);
+        if (tabs[j].classList.contains(data)) {
+          setTimeout(function () {
+            tabs[j].style.display = 'flex';
+          }, 750);
+          tabs[j].classList.add('active');
+        }
+      }
+
+      //       console.log(index);
+      for (let g = 0; g < elements.length; g++) {
+        elements[g].classList.remove('active');
+
+        if (elements[g].dataset.number === numbered) {
+          elements[g].classList.add('active');
+        }
+      }
+    });
+  }
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function (e) {
+      e.preventDefault();
+
+      for (let sibling of this.parentElement.parentElement.children) {
+        if (sibling !== this) {
+          sibling.childNodes[0].classList.remove('active');
+        }
+      }
+      this.classList.add('active');
+      data = this.dataset.anchor;
+      var numbered = this.dataset.number;
+      //       console.log(this.dataset.number);
+
+      for (let j = 0; j < tabs.length; j++) {
+        tabs[j].classList.remove('active');
+        setTimeout(function () {
+          tabs[j].style.display = 'none';
+        }, 750);
+        if (tabs[j].classList.contains(data)) {
+          setTimeout(function () {
+            tabs[j].style.display = 'flex';
+          }, 750);
+          tabs[j].classList.add('active');
+        }
+      }
+
+      //       console.log(index);
+      for (let g = 0; g < links.length; g++) {
+        links[g].classList.remove('active');
+
+        if (links[g].dataset.number === numbered) {
+          links[g].classList.add('active');
+        }
+      }
+    });
+  }
+}
