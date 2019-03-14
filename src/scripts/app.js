f5923192c9d8a5148965e4dd4fd3b2910065de64
ArkadiuszGarzel <ejkej87@gmail.com>
2019-03-14 21:11:27 +0100

    Add star color on hover

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,13 @@
 console.log('hello');
+
+var starContainer = document.getElementById('stars');
+var stars = Array.prototype.slice.call(starContainer.children);
+
+starContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  stars.forEach(function (el) {
+    return el.classList.remove('full');
+  });
+  e.target.classList.add('full');
+});
