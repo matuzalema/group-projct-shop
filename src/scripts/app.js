300219fc12d1ac700b3535734976518ca384292f
ArkadiuszGarzel <ejkej87@gmail.com>
2019-03-15 12:12:23 +0100

    Add star rating on products section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,42 @@
 console.log('hello');
 
-var starContainer = document.getElementById('stars');
-var stars = Array.prototype.slice.call(starContainer.children);
+let starContainer = document.querySelectorAll('.stars');
 
-starContainer.addEventListener('click', function (e) {
-  e.preventDefault();
-
+starContainer.forEach(function (item) {
+  const stars = item.querySelectorAll('a[title^="Rate"]');
   stars.forEach(function (el) {
-    return el.classList.remove('full');
+    el.addEventListener('click', rating);
+    el.addEventListener('mouseover', hoverEffect);
+    el.addEventListener('mouseout', hoverEffect);
   });
-  e.target.classList.add('full');
+
+  function rating (event) {
+    event.preventDefault();
+    const choice = event.target;
+    let active = false;
+    stars.forEach(function (star) {
+      if (active) {
+        star.classList.remove('active');
+        star.classList.remove('full');
+      } else if (!active) {
+        star.classList.add('active');
+      }
+      if (star === choice) {
+        active = true;
+      }
+    });
+  }
+
+  function hoverEffect (event) {
+    const choice = event.target;
+    let active = false;
+    stars.forEach(function (star) {
+      if (!active) {
+        star.classList.toggle('hoverEffect');
+      }
+      if (star === choice) {
+        active = !active;
+      }
+    });
+  }
 });
